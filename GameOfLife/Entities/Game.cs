b472e6d04bc53fffad79fed539a4dd9c74478f07
Ugnius Spaicys <ugnius.bootcamp@gmail.com>
2025-02-04 18:50:01 +0200

    removed lazy init

--- GameOfLife/Entities/Game.cs
+++ GameOfLife/Entities/Game.cs
@@ -7,26 +7,26 @@ namespace GameOfLife.Entities
         public int Generation { get; private set; } = 0;
         public IMap Map { get; private set; }
 
-        private readonly Lazy<IPrintable> _gamePrinter;
+        private readonly IPrintable _gamePrinter;
         private readonly IGameHandler _gameHandler;
 
         public Game(IMap map, IGameHandler gameHandler, Func<Game, IPrintable> printer)
         {
             Map = map;
-            _gamePrinter = new Lazy<IPrintable>(() => printer(this));
+            _gamePrinter = printer(this);
             _gameHandler = gameHandler;
         }   
 
         public void Run(int iterations, int delay = 1000)
         {
-            _gamePrinter.Value.Print();
+            _gamePrinter.Print();
 
             for (int i = 0; i <= iterations; i++)
             {
                 Map = _gameHandler.CalculateNextGeneration(Map);
                 Generation++;
 
-                _gamePrinter.Value.Print();
+                _gamePrinter.Print();
 
                 Thread.Sleep(delay);
             }
