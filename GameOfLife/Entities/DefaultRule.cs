e1f3e58a7723eebc402b3b82581baa1b2084134a
Ugnius Spaicys <ugnius.bootcamp@gmail.com>
2025-02-05 11:41:45 +0200

    added value constants

--- GameOfLife/Entities/DefaultRule.cs
+++ GameOfLife/Entities/DefaultRule.cs
@@ -4,18 +4,24 @@ namespace GameOfLife.Entities
 {
     public class DefaultRule : IRule
     {
+        private const int aliveNeighborCount = 2; // value for alive cell to stay alive
+        private const int aliveNeighborCount2 = 3; // value for alive cell to stay alive
+        private const int resurrectionNeighborCount3 = 3; // value for dead cell to become alive
+
+
         /// <summary>
         /// Determines if a cell should live or die based on the number of live neighbors
         /// </summary>
         /// <param name="isAlive">previous cell state</param>
         /// <param name="liveNeighbors">number of alive neighbors</param>
         /// <returns>cell is or is not alive</returns>
+        /// 
         public bool ShouldLive(bool isAlive, int liveNeighbors)
         {
             if (isAlive)
-                return (liveNeighbors == 2 || liveNeighbors == 3);
+                return (liveNeighbors == aliveNeighborCount || liveNeighbors == aliveNeighborCount2;
 
-            return liveNeighbors == 3;
+            return liveNeighbors == resurrectionNeighborCount3;
         }
     }
 }
