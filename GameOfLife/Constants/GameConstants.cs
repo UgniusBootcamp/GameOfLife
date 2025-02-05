8ad9dc84f16755d2b3a6426811852f64b70e1fe3
Ugnius Spaicys <ugnius.bootcamp@gmail.com>
2025-02-05 11:41:56 +0200

    summaries for constants

--- GameOfLife/Constants/GameConstants.cs
+++ GameOfLife/Constants/GameConstants.cs
@@ -8,19 +8,19 @@ namespace GameOfLife.Constants
 {
     public static class GameConstants
     {
-        public const char Alive = '0';
+        public const char Alive = '0'; // Alive cell representation
 
-        public const char Dead = ' ';
+        public const char Dead = ' '; // Dead cell representation
 
-        public const string LenghtInputMessage = "Enter the length of the map: ";
+        public const string LenghtInputMessage = "Enter the length of the map: "; // Message for inputting the length of the map
 
-        public const string HeightInputMessage = "Enter the height of the map: ";
-        
-        public const string GenerationsInputMessage = "Enter the number of generations: ";
-        
-        public const string GenerationMessage = "Generation: {0}";
+        public const string HeightInputMessage = "Enter the height of the map: "; // Message for inputting the height of the map
 
-        public const string PopulationMessage = "Population: {0}";
+        public const string GenerationsInputMessage = "Enter the number of generations: "; // Message for inputting the number of generations
+
+        public const string GenerationMessage = "Generation: {0}"; // Message for displaying the current generation
+
+        public const string PopulationMessage = "Population: {0}"; // Message for displaying the current population
 
     }
 }
